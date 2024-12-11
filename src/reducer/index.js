01430583e0d707913f5ed6c61dd540602e0c987b
shimssung <shimssung@gmail.com>
2024-12-11 18:03:03 +0900

    12/11 폴더 정리 완료

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,14 +1,12 @@
 import { combineReducers } from "redux";
 import AdminModal from "./AdminModal";
-import insightReducer from "./Insight";
 import HeaderBar from "./HeaderBar";
 import authSlice from "./authSlice";
 
 const rootReducer = combineReducers({
-    adminModal: AdminModal,
-    insightPage: insightReducer,
-    headerbar: HeaderBar,
-    auth: authSlice,
+  adminModal: AdminModal,
+  headerbar: HeaderBar,
+  auth: authSlice,
 });
 
 export default rootReducer;
