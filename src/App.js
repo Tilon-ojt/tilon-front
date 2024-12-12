2490b17dc9625e063f1ef5c6b2d33502692ea577
leehanse <prsuw113@naver.com>
2024-12-12 16:27:23 +0900

    fix: app.js 오류 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store"; // 또는 store가 정의된 파일의 경로
 import UserListPage2 from "./pages/Admin/UserListPage/UserListPage2";
 import EditProfile from "./pages/Admin/EditProfile/EditProfile";
-import AdminInsight from "./pages/Admin/Insight/AdminInsight";
 import NewsDetail from "./pages/Admin/NewsPage/NewsDetail";
 import NewsCreate from "./pages/Admin/NewsPage/NewsCreate";
 import PostWrite from "./pages/Admin/PostEdit/PostWrite";
@@ -9,17 +10,12 @@ import AdminNews from "./pages/Admin/NewsPage/AdminNews";
 import NewsEdit from "./pages/Admin/NewsPage/NewsEdit";
 import TilonHomepage from "./pages/Home/TilonHomepage";
 import Loginpage from "./pages/Admin/Login/Loginpage";
-import EditDetail from "./pages/Admin/Pr/EditDetail";
 import Sidebar from "./components/common/Sidebar";
 import PrivateRoute from "./routes/PrivateRoute";
 import Navbar from "./components/common/Navbar";
 import AdminPr from "./pages/Admin/Pr/AdminPr";
 import AdminInsight from "./pages/Admin/Insight/AdminInsight";
-import PrivateRoute from "./components/PrivateRoute";
-import UserListPage2 from "./pages/Admin/UserListPage/UserListPage2";
 import PostEdit from "./pages/Admin/PostEdit/PostEdit";
-import EditDetail from './pages/Admin/Pr/EditDetail';
-import PostWrite from "./pages/Admin/PostEdit/PostWrite";
 
 function App() {
   return (
